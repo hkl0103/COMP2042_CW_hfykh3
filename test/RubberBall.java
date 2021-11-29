bc76061662e2cc910c5ed49926f977bdfa6c9acd
hkl0103 <kellyhee100@gmail.com>
2021-11-30 00:03:15 +0800

    move ball upwards which on the bar

--- test/RubberBall.java
+++ test/RubberBall.java
@@ -24,8 +24,8 @@ import java.awt.geom.Point2D;
 public class RubberBall extends Ball {
 
 
-    private static final int DEF_RADIUS = 10;
-    private static final Color DEF_INNER_COLOR = new Color(255, 219, 88);
+    private static final int DEF_RADIUS = 15;
+    private static final Color DEF_INNER_COLOR = new Color(210, 199, 199);
     private static final Color DEF_BORDER_COLOR = DEF_INNER_COLOR.darker().darker();
 
 
@@ -37,8 +37,8 @@ public class RubberBall extends Ball {
     @Override
     protected Shape makeBall(Point2D center, int radiusA, int radiusB) {
 
-        double x = center.getX() - (radiusA / 2);
-        double y = center.getY() - (radiusB / 2);
+        double x = center.getX() - (radiusA / 1);
+        double y = center.getY() - (radiusB / 1);
 
         return new Ellipse2D.Double(x,y,radiusA,radiusB);
     }
